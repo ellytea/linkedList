ede3d5e28d7180a51f3f905f4ee32ba454508aa3
elly torres <nallelyt4@gmail.com>
2018-08-28 07:55:15 -0600

    Change read to red

--- scripts.js
+++ scripts.js
@@ -1,33 +1,42 @@
-//Variables
-
+// Variables
 
 var bookmarkTitle = document.querySelector('#enter-website');
 var bookmarkURL = document.querySelector('#enter-url');
 var createBookmark = document.querySelector('.create-bookmark');
 var displayTitle = document.querySelector('.site-title');
 var displayURL = document.querySelector('.url');
-var readBookmark = document.querySelector('.read');
+var readBookmarks = document.querySelectorAll('.read-button');
 var deleteBookmark = document.querySelector('.delete');
-
 var cardContainer = document.querySelector('.cards-container');
 
 
-//Event Listeners
+// Event Listeners
 createBookmark.addEventListener('click', makeBookmark);
-// readBookmark.addEventListener('click', readBookmark);
 // deleteBookmark.addEventListener('click', deleteBookmark);
 
 
-
-//functions
+// functions
 function makeBookmark(event){
   event.preventDefault();
   var newLi = document.createElement('li');
   newLi.innerHTML = `<div class="one">
         <p class="site-title"> ${bookmarkTitle.value}</p>
         <p class="url"> ${bookmarkURL.value}</p>
-        <p class="read">Read<span class="delete">Delete</span></p>
+        <p class="read-button">Read<span class="delete">Delete</span></p>
       </div>`
+  newLi.addEventListener('click', markRead);
   cardContainer.prepend(newLi);
 }
+
+
+function markRead(e) {
+  	e.target.classList = ['read']
+ }
+
+
+
+
+
+
+
 
