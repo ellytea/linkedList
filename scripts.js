2222922a41e630b5cb89dfa61747e22d3cc82661
Cole Vanacore <cole.vanacore@coloradocollege.edu>
2018-08-29 16:55:41 -0600

    Add alert message for invalid URL

--- scripts.js
+++ scripts.js
@@ -9,21 +9,31 @@ var cardContainer = document.querySelector('.cards-container');
 var numCount = document.querySelector('.num-count');
 var counter = 0;
 var readCount = document.querySelector('.read-count');
-
+var clearReadBookmarksButton = document.querySelector('.clear-read-bookmarks');
 
 
 // Event Listeners
 createBookmark.addEventListener('click', makeBookmark);
 bookmarkURL.addEventListener('keyup', blankInput);
 bookmarkTitle.addEventListener('keyup', blankInput);
+clearReadBookmarksButton.addEventListener('click', clearBookmarks);
 
 // functions
+
+function validateURL() {
+    var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+    return urlregex.test(bookmarkURL.value);
+}
+
+
+
 function makeBookmark(event){
-  event.preventDefault();
+event.preventDefault();
+  if (validateURL()){
   var newLi = document.createElement('li');
   newLi.innerHTML = `<div class="one">
         <p class="site-title"> ${bookmarkTitle.value}</p>
-        <a href="https://${bookmarkURL.value}" target="_blank" class="url"> ${bookmarkURL.value}</a>
+        <a href="${bookmarkURL.value}" target="_blank" class="url"> ${bookmarkURL.value}</a>
         <p class="read-button"> Read </p> 
         <p class="delete"> Delete </p>
       </div>`
@@ -44,8 +54,12 @@ function makeBookmark(event){
   counter++ ;
   numCount.innerText ='Number of Bookmarks: ' + counter;
   countBookmarks();
+  }
+  else{ 
+    alert('This is not a valid URL!')
+    }
+  }
 
-}
 
 function markRead(){
   if (this.parentNode.classList.contains('read') === false){
@@ -59,17 +73,31 @@ function markRead(){
     countBookmarks();
 }
 
-function blankInput(){
+function blankInput() {
   if (bookmarkURL.value ==='' || bookmarkTitle.value ==='')
   {
     createBookmark.disabled = true;
+    clearReadBookmarksButton.disabled = true;
   }
   else{
     createBookmark.disabled = false;
+    clearReadBookmarksButton.disabled = false;
   }
 }
 
-function countBookmarks(){
-  var numberOfReadBookmarks = document.querySelectorAll('.read');
-  readCount.innerText = `Bookmarks Read: ${numberOfReadBookmarks.length} Bookmarks Unread: ${counter - numberOfReadBookmarks.length}`;
+function countBookmarks() {
+  var totalReadBookmarks = document.querySelectorAll('.read');
+  readCount.innerText = `Bookmarks Read: ${totalReadBookmarks.length} Bookmarks Unread: ${counter - totalReadBookmarks.length}`;
+  numCount.innerText ='Number of Bookmarks: ' + counter;
 }
+
+function clearBookmarks() {
+  var totalReadBookmarks = document.querySelectorAll('.read');
+    for (var i =0; i < totalReadBookmarks.length; i++){
+      totalReadBookmarks[i].remove();
+      counter-- ;
+      } 
+      countBookmarks();
+  }
+
+
